80ec05c14a5e5b712f15b717104a4849ae2b727c
Etienne Lardeur <etienne.lardeur@gmail.com>
2021-02-02 22:55:03 +0100

    remove useless files

--- P8_bootstrap.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-sudo python3 -m pip install --upgrade pip
-sudo python3 -m pip install pillow
-sudo python3 -m pip install pandas
-sudo python3 -m pip install tensorflow
-sudo python3 -m pip install pyarrow
-sudo python3 -m pip install boto3
